6c892778706090eef03484bed4fcf51e0edf9d29
HaKIM <webdevhakim@gmail.com>
2022-12-07 15:46:36 +0100

    Changed events selection algorithm

--- src/main/kotlin/com/hakim/hakimbot/gamble/event/EventType.kt
+++ src/main/kotlin/com/hakim/hakimbot/gamble/event/EventType.kt
@@ -1,10 +1,10 @@
 package com.hakim.hakimbot.gamble.event
 
-enum class EventType {
-    VERY_POSITIVE,
-    POSITIVE,
-    NORMALLY_POSITIVE,
-    NORMALLY_NEGATIVE,
-    NEGATIVE,
-    VERY_NEGATIVE
+enum class EventType(val value: Int) {
+    VERY_POSITIVE(3),
+    POSITIVE(2),
+    NORMALLY_POSITIVE(1),
+    NORMALLY_NEGATIVE(-1),
+    NEGATIVE(-2),
+    VERY_NEGATIVE(-3)
 }
